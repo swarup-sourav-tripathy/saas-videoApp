f8c547408db9ad2f53824de58df5040016e6ce02
swarup <swaruptripathy03@gmail.com>
2025-01-05 01:20:01 +0530

    some deployment issues testing

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -4,16 +4,19 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export async function GET(request: NextRequest){
+export async function GET(request: NextRequest) {
     try {
         const videos = await prisma.video.findMany({
-            orderBy: {createdAt: "desc"}
+            orderBy: { createdAt: "desc" }
         })
-        console.log(videos,"test");
-        
+        console.log(videos, "test");
+
+        const headers = new Headers();
+        headers.set("Cache-Control", "no-store");
+
         return NextResponse.json(videos)
     } catch (error) {
-        return NextResponse.json({error: "Error fetching videos"}, {status: 500})
+        return NextResponse.json({ error: "Error fetching videos" }, { status: 500 })
     } finally {
         await prisma.$disconnect()
     }
