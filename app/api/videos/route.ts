d82d39488d3c92450c149493bf2b0db77067d672
swarup <swaruptripathy03@gmail.com>
2025-01-05 01:56:42 +0530

    testing at its end

--- app/api/videos/route.ts
+++ app/api/videos/route.ts
@@ -1,8 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client"
-
-
-const prisma = new PrismaClient()
+import prisma from "@/prisma/lib/prisma";
 
 export async function GET(request: NextRequest) {
     try {
