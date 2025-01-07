5f1179316c6008ced344eabb04a355b67b022262
swarup <swaruptripathy03@gmail.com>
2025-01-08 03:46:03 +0530

    trying

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,27 @@ const nextConfig = {
     images: {
       domains: ['res.cloudinary.com'],
     },
+    async headers() {
+      return [
+        {
+          source: "/(.*)", // Apply headers to all routes
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+            },
+            {
+              key: "Pragma",
+              value: "no-cache",
+            },
+            {
+              key: "Expires",
+              value: "0",
+            },
+          ],
+        },
+      ];
+    },
   };
 
 
