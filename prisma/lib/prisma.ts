cbdd41a0896bf224217502bcb2c0c2aceb1378ad
swarup <swaruptripathy03@gmail.com>
2025-01-08 03:24:16 +0530

    prisma file updated

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -1,15 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma =
+  process.env.NODE_ENV === 'development'
+    ? global.prisma || (global.prisma = new PrismaClient())
+    : new PrismaClient();
 
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
